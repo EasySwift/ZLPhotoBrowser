d992b300d5eba8e3b9c09bacac02d011acb8b7bb
stubbornnessness <1219721814yxj>
2016-10-10 22:44:21 +0800

    1.2版本开始支持carthage

---
+++ ZLPhotoBrowser_iOS/ZLPhotoBrowser_iOS.h
@@ -0,0 +1,19 @@
+//
+//  ZLPhotoBrowser_iOS.h
+//  ZLPhotoBrowser_iOS
+//
+//  Created by yuanxiaojun on 2016/10/10.
+//  Copyright © 2016年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZLPhotoBrowser_iOS.
+FOUNDATION_EXPORT double ZLPhotoBrowser_iOSVersionNumber;
+
+//! Project version string for ZLPhotoBrowser_iOS.
+FOUNDATION_EXPORT const unsigned char ZLPhotoBrowser_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZLPhotoBrowser_iOS/PublicHeader.h>
+
+
